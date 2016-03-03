f36d5016f337e21afbf38c7a02f00ec912acb996
arial-alisha <aria0023@algonquinlive.com>
2016-03-02 20:45:29 -0500

    attempt at looping

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var name = prompt('Enter a letter');
+
+var counter;
+
+for (counter = 0; counter < 10; counter++) {
+  document.write(name + '<br>');
+}
+
+for (counter = 1; counter < 10; counter++) {
+  document.write(name + '<br>');
+}
+
+for (counter = 2; counter < 10; counter++) {
+  document.write(name + '<br>');
+}
