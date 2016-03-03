9168bccbb94592b794fcd22e16b642e0b0241793
arial-alisha <aria0023@algonquinlive.com>
2016-03-03 12:18:38 -0500

    infix operators

--- js/main.js
+++ js/main.js
@@ -3,11 +3,10 @@ var letter = prompt('Enter a letter');
 var i;
 var j;
 
-
 for (i = 0; i < 10; i++) {
-  document.write(letter);
+  document.write (letter);
   for (j = 0; j <i; j++) {
-    document.write(letter);
+    document.write (letter);
   }
 
   document.write('<br>');
